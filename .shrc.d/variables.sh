704ea1b25f1aea6d0c938913c504e7a466dc99fe
john-craig <john.e.craig.jr@gmail.com>
2024-08-10 08:47:07 -0400

    Make default editor emacs

--- .shrc.d/variables.sh
+++ .shrc.d/variables.sh
@@ -2,6 +2,8 @@ export PS1=""
 export TOR_DOWNLOADS="$HOME/.local/opt/tor-browser/app/Browser/Downloads"
 export NIX_REMOTE=daemon
 
+export EDITOR="emacs -nw"
+
 # LaTeX varibles
 TEXMFDIST=/usr/share/texmf-dist
 TEXMFHOME=~/.texmf
