7fe6e58d84de1d5c6ed1c3e0693955b2e23cd731
john-craig <john.e.craig.jr@gmail.com>
2024-08-11 11:50:34 -0400

    Add deploy-host script

--- .shrc.d/variables.sh
+++ .shrc.d/variables.sh
@@ -7,3 +7,10 @@ export EDITOR="emacs -nw"
 # LaTeX varibles
 TEXMFDIST=/usr/share/texmf-dist
 TEXMFHOME=~/.texmf
+
+# Paths for different programming projects
+export HOMELAB_CONFIGURATIONS="$HOME/programming/by_category/homelab/homelab-configurations"
+export NIXPKGS_APOCRYPHA="$HOME/programming/by_category/homelab/nixpkgs-apocrypha"
+
+export GALLIPEDAL_SERVICES="$HOME/programming/by_category/selfhosting/services-library"
+export GALLIPEDAL_MODULE="$HOME/programming/by_category/selfhosting/gallipedal-module"
