88eeba382ed27dd0d460950707f9dcf433652eb2
john-craig <john.e.craig.jr@gmail.com>
2024-08-24 20:45:33 -0400

    Add nix binaries to PATH

--- .shrc.d/path.sh
+++ .shrc.d/path.sh
@@ -1,5 +1,6 @@
 PATH="/opt/android-sdk/cmdline-tools/latest/bin/:$PATH"
 PATH="$PATH:$HOME/.local/bin"
+PATH="$PATH:$HOME/.nix-profile/bin"
 export PATH="$HOME/.pyenv/bin:$PATH"
 #eval "$(pyenv init -)"
 #eval "$(pyenv virtualenv-init -)"
